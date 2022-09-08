a616e6982d5a6a361ce388838d37c84332918ffc
pauls3 <paulstanik3@gmail.com>
2022-09-08 16:22:16 -0700

    working on tutorial

---
+++ API-Tutorial/Controllers/DepartmentController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using API_Tutorial.Models;
+
+namespace API_Tutorial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public DepartmentController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // API method to get department details
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"
+                           select DepartmentId, DepartmentName from dbo.Department";
+
+            DataTable table = new DataTable();
+
+            // variable that stores database connection string
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using(SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+
+
+        // Insert Data into db -> department table
+        [HttpPost]
+
+        public JsonResult Post(Department dep)
+        {
+            string query = @"
+                           insert into dbo.Department values
+                           ('"+dep.DepartmentName+@"')
+                           ";
+
+            DataTable table = new DataTable();
+
+            // variable that stores database connection string
+            string sqlDataSource = _configuration.GetConnectionString("EmployeeAppCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+    }
+}
---
+++ API-Tutorial/Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace API_Tutorial.Models
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public string Department { get; set; }
+        public string DateOfJoining { get; set; }
+        public string PhotoFileName { get; set; }
+
+    }
+}
